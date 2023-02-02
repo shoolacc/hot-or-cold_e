04892d9eda7475529739fba75e3cf97b7a193702
shoolacc <123706564+shoolacc@users.noreply.github.com>
2023-02-02 11:33:27 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,48 @@
+let P = 0
+let level = 0
+input.onPinPressed(TouchPin.P0, function () {
+    P = randint(0, 99)
+    basic.showNumber(P)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.magneticForce(Dimension.X))
+})
+input.onButtonPressed(Button.B, function () {
+    level = input.lightLevel()
+    basic.showNumber(level)
+})
+basic.forever(function () {
+    if (input.temperature() < 18) {
+        basic.showNumber(input.temperature())
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    if (input.temperature() >= 18 && input.temperature() <= 24) {
+        basic.showNumber(input.temperature())
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (input.temperature() <= 24) {
+        basic.showNumber(input.temperature())
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+    }
+})
